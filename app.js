989fab19f7d0bee56b9953bd117465ea975634dd
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-11-07 22:46:29 +0400

    implement prisma and set up auth routes

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import postRoute from "./routes/post.js";
+import authRoute from "./routes/auth.js";
 dotenv.config();
 
 const app = express();
 
-app.use("/api/test", (req, res) => {
-  res.send("yayyyy");
-});
+app.use(express.json());
+
+app.use("/api/posts", postRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(process.env.DEV_PORT || 8000, () => {
   console.log(`Server is running on port ${process.env.DEV_PORT || 8000}`);
