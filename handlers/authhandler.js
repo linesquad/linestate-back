a747f5d32feee071f18c71ca042863eb730e7f4f
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-11-23 17:52:43 +0400

    fix

--- handlers/authhandler.js
+++ handlers/authhandler.js
@@ -65,11 +65,12 @@ export const login = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        // secure: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax",
         maxAge: max,
       })
       .status(200)
-      .json(userWithoutPassword);
+      .json({ user: userWithoutPassword });
 
     // db
   } catch (error) {
